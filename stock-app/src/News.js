d70d296385339f258cfb963d51fa83527143123b
Ioan Cirja <ioan.cirja.00@gmail.com>
2023-09-14 14:38:24 +0300

    10 merged news starting css

--- stock-app/src/News.js
+++ stock-app/src/News.js
@@ -1,88 +1,57 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import "./News.css";
 
 function News() {
-  const [marketNews, setMarketNews] = useState([]);
-  const [companyNews, setCompanyNews] = useState([]);
-  const [loadingMarketNews, setLoadingMarketNews] = useState(true);
-  const [loadingCompanyNews, setLoadingCompanyNews] = useState(true);
+  const [news, setNews] = useState([]);
+  const [loadingNews, setLoadingNews] = useState(true);
 
   useEffect(() => {
     const apiKey = process.env.REACT_APP_FINHUB_API_KEY;
 
-    const fetchMarketNews = async () => {
-      try {
-        const response = await axios.get(
-          `https://finnhub.io/api/v1/news?category=general&token=${apiKey}`
-        );
+    const fetchMarketNews = axios.get(
+      `https://finnhub.io/api/v1/news?category=general&token=${apiKey}`
+    );
 
-        if (response.data && response.data.length > 0) {
-          setMarketNews(response.data.slice(0, 10));
-          setLoadingMarketNews(false);
-        }
-      } catch (error) {
-        console.error("Error fetching market news:", error);
-        setLoadingMarketNews(false);
-      }
-    };
+    const fetchCompanyNews = axios.get(
+      `https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2023-08-15&to=2023-08-20&token=${apiKey}`
+    );
 
-    const fetchCompanyNews = async () => {
-      try {
-        const response = await axios.get(
-          `https://finnhub.io/api/v1/company-news?symbol=AAPL&from=2023-08-15&to=2023-08-20&token=${apiKey}`
-        );
-
-        if (response.data && response.data.length > 0) {
-          setCompanyNews(response.data.slice(0, 10));
-          setLoadingCompanyNews(false);
-        }
-      } catch (error) {
-        console.error("Error fetching company news:", error);
-        setLoadingCompanyNews(false);
-      }
-    };
-
-    fetchMarketNews();
-    fetchCompanyNews();
+    Promise.all([fetchMarketNews, fetchCompanyNews])
+      .then(([marketResponse, companyResponse]) => {
+        const marketNews = marketResponse.data.slice(0, 5);
+        const companyNews = companyResponse.data.slice(0, 5);
+        const mergedNews = [...marketNews, ...companyNews];
+        setNews(mergedNews);
+        setLoadingNews(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching news:", error);
+        setLoadingNews(false);
+      });
   }, []);
 
   return (
-    <div className="News">
-      <h1>News Page</h1>
-      <Link to="/">
-        <button>Retun to Home</button>
-      </Link>
+    <div>
 
-      <div>
-        <h2>Market News</h2>
-        {loadingMarketNews ? (
-          <p>Loading market news...</p>
-        ) : (
-          <ul>
-            {marketNews.map((newsItem) => (
-              <li key={newsItem.id}>
-                <a
-                  href={newsItem.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {newsItem.headline}
-                </a>
-                <p>{newsItem.summary}</p>
-              </li>
-            ))}
-          </ul>
-        )}
+      <div className="header">
+        <h1>News Page</h1>
+        <Link to="/">
+          <button>Return to Home</button>
+        </Link>
+
+
       </div>
 
-      <div>
-        <h2>Company News</h2>
-        {loadingCompanyNews ? (
-          <p>Loading company news...</p>
+
+      <div className="News">
+        <h2>Company & Market News</h2>
+        {loadingNews ? (
+          <p>Loading news...</p>
         ) : (
           <ul>
-            {companyNews.map((newsItem) => (
+            {news.map((newsItem) => (
               <li key={newsItem.id}>
                 <a
                   href={newsItem.url}
