66480f74190dc786b5fc28f9d25b05fe9ebc1abe
Hapau Andreea <119053581+andreeahapau@users.noreply.github.com>
2023-09-16 16:14:12 +0300

    Update App.js

--- stock-app/src/App.js
+++ stock-app/src/App.js
@@ -15,16 +15,32 @@ function App() {
 
 function Home() {
   return (
-    <div className="header">
-      <div className="left">
-        <img src="/stock-logo.svg" alt="Stock Tracker Logo" />
-        <h1>Stock Tracker</h1>
+    <div className="App">
+      <div className="header">
+        <div className="left">
+          <img src="/stock-logo.svg" alt="Stock Tracker Logo" />
+          <h1>Stock Tracker</h1>
+        </div>
+        <div className="right">
+          <Link to="/news">
+            <button>News</button>
+          </Link>
+        </div>
       </div>
-      <div className="right">
-        <Link to="/news">
-          <button>News</button>
-        </Link>
-      </div>
+
+      <main>
+        <h1 id="h1Main">The World’s Leading <br></br>
+          Cryptocurrency Platform</h1>
+
+        <h2 id="h2Main">Buy Bitcoin, Ethereum, and all your favourite crypto</h2>
+
+        <ul id="mainList">
+          <li>✔ Trusted by more than 80M users world-wide</li>
+          <li> ✔ Leader in regulatory compliance and security certifications</li>
+          <li> ✔ The industry’s most comprehensive insurance coverage and verified proof of reserves</li>
+        </ul>
+
+      </main>
     </div>
   );
 }
