bb28a16a9cd8aeffc11dbe74550341079094e5c2
Hapau Andreea <119053581+andreeahapau@users.noreply.github.com>
2023-09-20 09:46:43 +0300

    Update SignupForm.js

--- stock-app/src/SignupForm.js
+++ stock-app/src/SignupForm.js
@@ -1,39 +1,64 @@
 import React, { useState } from 'react';
+import "./Home.css";
 
 function SignupForm({ isOpen, onRequestClose }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  
+
     console.log('Login with:', username, 'and password:', password);
-  
+
+    if (username && password) {
+      setIsLoggedIn(true);
+    } else {
+      alert('Please fill in both username and password.');
+    }
+  };
+
+  const handleClose = () => {
+    setIsLoggedIn(false);
+    setUsername('');
+    setPassword('');
     onRequestClose();
   };
 
   return (
     <div className={`signup-form ${isOpen ? 'open' : ''}`}>
-      <dialog id="dialogLogin"onSubmit={handleSubmit}>
-        <h2 id ="h2Dialog">Access the Cryptoverse</h2>
-        <div>
-          <label>Username </label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+      {isLoggedIn ? (
+        <div id="output">
+          <p id="dialog-text">Step into the Crypto World: Your Best Decision Today!</p>
+          <img src="/crypto.svg" id="crypto-logo" alt="crypto logo" />
+          <button id="start-button" onClick={handleClose}>Explore</button>
         </div>
-        <div>
-          <label>Password   </label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" id = "loginButton">Login</button>
-      </dialog>
+      ) : (
+        <dialog id="dialogLogin" open={isOpen}>
+          <h2 id="h2Dialog">Access the Cryptoverse</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>Username</label>
+              <input
+                type="email"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit" id="loginButton">
+              {isLoggedIn ? 'Welcome!' : 'Login'}
+            </button>
+          </form>
+        </dialog>
+      )}
     </div>
   );
 }
